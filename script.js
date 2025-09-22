ad99ac31f26d41c56991e0ad3c3b52c448bbda44
sistemes_informatics <eros.horno@mataro.epiaedu.cat>
2025-09-22 20:01:45 +0200

    cambios

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 onload = () => {
   document.body.classList.remove("container");
 };
+    const abrirBtn = document.getElementById('abrirBtn');
+    const intro = document.getElementById('intro');
+    const regalo = document.getElementById('regalo');
+
+    abrirBtn.addEventListener('click', () => {
+      // Oculta el contenedor inicial
+      intro.classList.add('oculto');
+      // Muestra el segundo con una pequeña demora para que se vea la transición
+      setTimeout(() => {
+        regalo.classList.remove('oculto');
+      }, 300);
+    });
